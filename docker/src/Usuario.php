ca896c0cf93b6b7b71713fe058b35fd662f77993
Jxse111 <josemartinezestrada111@gmail.com>
2025-02-08 10:22:49 +0100

    Ajustes Clase Usuario

--- docker/src/Usuario.php
+++ docker/src/Usuario.php
@@ -1,17 +1,17 @@
 <?php
-
+require_once('conexiónBD/Algrano.php');
 /**
  * Clase que implementa un objeto de tipo Usuario para el acceso a la web.
  *
  * @author José Martínez Estrada
  */
-abstract class Usuario
+class Usuario
 {
     private $nombre;
     private $contrasena;
     private $direccion;
     private $correo;
-    private DateTime $fechaNacimiento;
+    private $fechaNacimiento;
 
     /**
      * Constructor que crea un usuario.
@@ -19,14 +19,70 @@ abstract class Usuario
      * @param mixed $contrasena contraseña del usuario
      * @param mixed $direccion dirección de la localización del usuario
      * @param mixed $correo correo electrónico del usuario
-     * @param DateTime $fechaNacimiento fehca de nacimiento del usuario
+     * @param mixed $fechaNacimiento fehca de nacimiento del usuario
      */
-    public function __construct($nombre, $contrasena, $direccion, $correo, DateTime $fechaNacimiento)
+    public function __construct($dni, $nombre, $contrasena, $direccion, $correo, $fechaNacimiento)
     {
+        $this->dni = $dni;
         $this->nombre = $nombre;
         $this->contrasena = $contrasena;
         $this->direccion = $direccion;
         $this->correo = $correo;
         $this->fechaNacimiento = $fechaNacimiento;
+    }
+
+    //Métodos getter y setter
+
+    public function getDni()
+    {
+        return $this->dni;
+    }
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function getContrasena()
+    {
+        return $this->contrasena;
+    }
+    public function getdireccion()
+    {
+        return $this->direccion;
+    }
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+    public function getFechaNacimiento()
+    {
+        return $this->fechaNacimiento;
+    }
+
+    //Métodos de la estáticos
+    public function guardarUsuario()
+    {
+        $conexionBD = Algrano::conectarAlgranoMySQLi();
+        $esValido = false;
+        $dniUsuario = $this->dni;
+        $nombreUsuario = $this->nombre;
+        $contraseñaUsuario = $this->contrasena;
+        $direccionUsuario = $this->direccion;
+        $correoUsuario = $this->correo;
+        $fechaNacUsuario = $this->fechaNacimiento;
+        if (noExisteUsuario($dni, $conexionBD)) {
+            $consultaInsercionUsuario = $conexionBD->prepare('INSERT INTO usuario VALUES (?,?,?,?,?,?)');
+            $consultaInsercionUsuario->bind_param('ssssss', $dniUsuario, $nombreUsuario, $contraseñaUsuario, $direccionUsuario, $correoUsuario, $fechaNacUsuario);
+            if ($consultaInsercionUsuario->execute()) {
+                $esValido = true;
+            }
+        } else {
+            $consultaInsercionUsuario = $conexionBD->prepare('UPDATE usuario SET nombre = ? , contraseña = ?, direccion = ?, correo = ?, fecha_nac = ? WHERE DNI = ?');
+            $consultaInsercionUsuario->bind_param('ssssss', $nombreUsuario, $contraseñaUsuario, $direccionUsuario, $correoUsuario, $fechaNacUsuario, $dniUsuario);
+            if ($consultaInsercionUsuario->execute()) {
+                $esValido = true;
+            }
+        }
+        return $esValido ? true : false;
     }
 }
